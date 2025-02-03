a744c6e73fb66e221e33e6c11d1dac60f0a2e612
AMAR DEEP s <138798428+amargauge26@users.noreply.github.com>
2025-02-03 16:58:06 +0530

    Time: 8 ms (69.21%) | Memory: 13.6 MB (54.76%) - LeetSync

---
+++ 205-isomorphic-strings/isomorphic-strings.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def isIsomorphic(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        if len(set(s))!=len(set(t)):
+            return False
+        
+        hash={}
+
+        for i in range(len(s)):
+            if s[i] not in hash:
+                hash[s[i]] = t[i]
+            
+            else:
+                if hash[s[i]]!=t[i]:
+                    return False
+                
+                else:
+                    hash[s[i]]=t[i]
+        
+        return True
