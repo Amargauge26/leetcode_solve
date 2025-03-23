38f606d529911e47cb09c32933682a32537bc931
Amar Deep Singh <138798428+amargauge26@users.noreply.github.com>
2025-03-23 17:21:26 +0530

    Time: 0 ms (100.00%) | Memory: 17.8 MB (33.90%) - LeetSync

--- 75-sort-colors/sort-colors.py
+++ 75-sort-colors/sort-colors.py
@@ -4,7 +4,7 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        d = Counter(nums)
+        """d = Counter(nums)
 
 
         for i in range(d[0]):
@@ -14,4 +14,30 @@ class Solution:
         
         for k in range(d[0]+d[1],d[0]+d[1]+d[2]):
             nums[k]=2
-        return nums
+        return nums"""
+
+        #ducthnational flag aklgo
+
+        """n = len(nums)
+        low =0
+        mid =0
+        high = n-1
+
+        while mid<=high:
+            if nums[mid]==0:
+                nums[mid],nums[low]=nums[low],nums[mid]
+                low+=1
+                mid+=1
+            
+            elif nums[mid]==1:
+                mid+=1
+            
+            else:
+                nums[mid],nums[high]=nums[high],nums[mid]
+                high-=1
+        
+
+        return nums"""
+        nums.sort()
+                                                                        
+
